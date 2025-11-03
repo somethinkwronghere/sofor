e69156dcaa01045c2da8a23c8f84ae92b4c626ad
somethinkwornghere <b200101025@subu.edu.tr>
2025-11-03 10:48:35 +0300

    Use production settings by default in WSGI/ASGI; Render hosts/CSRF

--- gorev_takip/asgi.py
+++ gorev_takip/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gorev_takip.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gorev_takip.settings_production')
 
 application = get_asgi_application()
--- gorev_takip/wsgi.py
+++ gorev_takip/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gorev_takip.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gorev_takip.settings_production')
 
 application = get_wsgi_application()
